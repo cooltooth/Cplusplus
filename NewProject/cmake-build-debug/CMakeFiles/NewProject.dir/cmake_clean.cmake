393a07c18af6d3884a377d12d363c64c08f47f4c
Daniel Hazelton <xooltooth@gmail.com>
2018-02-01 22:31:40 -0800

    everything now

---
+++ NewProject/cmake-build-debug/CMakeFiles/NewProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NewProject.dir/main.cpp.o"
+  "NewProject.pdb"
+  "NewProject.exe"
+  "libNewProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NewProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NewProject/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    //nothing in here
+}
