74a4bb354cafe33fd2d7c9ee55d08c4a5a138a62
Daniel Hazelton <xooltooth@gmail.com>
2018-02-05 19:42:26 -0800

    feb 5

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,7 +1,122 @@
+/*
+ * THE AXE
+ * *      *
+ * ** ** **
+ * ********
+ * ** ** **
+ * *  **  *
+ *    **
+ *    **
+ *    **
+ *    **
+ * Also, screw you even numbers
+ */
+
 #include <iostream>
+#include <limits>
 using std::cout;
 using std::cin;
 using std::endl;
+
+int main() {
+
+    int bladeHeight;
+
+    while (1)
+    {
+        cout << "Please enter blade height: ";
+        cin >> bladeHeight;
+
+        if(bladeHeight>0) {
+            break;
+        }else{
+            cout << "Please enter a valid positive integer" << endl;
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+
+    bool mid            = false;                    // Tracker for mid point of blade.
+    bool handleTrigger  = false;                    // Trigger for handle...
+    int  handleCounter  = 0;                        // Tracker for handle length.
+
+    for(int r=1; r > 0 || handleCounter < 10; mid ? r-- : r++)
+    {
+        bool halfPoint = false;
+
+        if(r >= (bladeHeight / 2) + 1)
+            mid = true;
+
+        for(int c=0; c>=0; halfPoint ? --c : c++)
+        {
+            cout << (c < r ? '*' : ' ');
+
+            if(c == bladeHeight / 2 && !halfPoint)
+            {
+                halfPoint       = true;
+                handleTrigger   = true;
+                c++;
+            }
+
+            if (handleTrigger)
+            {
+                // Handle check, row is Bladeheight/2(int rounding) and handle hasn't started/ended yet
+                if (r == bladeHeight / 2 || handleCounter >= 1 && handleCounter < 10) {
+                    cout << "**";
+                    handleCounter++;
+                } else {
+                    cout << "  ";
+                }
+                handleTrigger = false;
+            }
+        }
+        cout<<endl;                                         // Next row
+    }
+    return 0;
+}
+
+/*  Alternative multi-loop version
+ *
+ * for(int r=1;r>0 || handleEnd<10;(mid)?r--:r++){
+        // Flip blade building direction when mid point is reached
+        if(r>=(bladeHeight/2)+1){
+            mid = true;
+        }
+
+        //left side of the blade
+        for(int c1=1;c1<=(bladeHeight/2)+1;c1++){
+            if(c1<=r) {
+                cout << '*';
+            }else{
+                cout << ' ';
+            }
+        }
+
+        // Handle check, row is Bladeheight/2(int rounding) and handle hasn't started/ended yet
+        if(r==(bladeHeight/2)&&!(handleEnd>1&&handleEnd<10)){
+            cout<<"**";
+            handleEnd++;
+        }else if(handleEnd>=1&&handleEnd<10){
+            cout<<"**";
+            handleEnd++;
+        }else{
+            cout<<"  ";
+        }
+
+        // Right side of the blade
+        for(int c3=(bladeHeight/2)+1;c3>0;c3--){
+            if(c3>r) {
+                cout << ' ';
+            }else{
+                cout << '*';
+            }
+        }
+                cout<<endl;                                         // Next row
+    }
+
+*/
+
+
 
 /* FIZZBUZZ
 int main(){
@@ -26,6 +141,8 @@ int main(){
     char x;
     std::cin>>x;
 }*/
+
+
 
 /* Reverse a string or sentence
 int main(){
@@ -151,21 +268,60 @@ int main() {
 /*
  * Given a string of expressions (only variables, +, and -) and a set of variable/value pairs (i.e. a=1, b=7, c=3, d=14) return the result of the expression ("a + b+c -d" would be -3).
  *
-#include <string>
+#include <vector>
+using std::vector;
+#include <sstream>
+using std::istringstream;
+using std::getline;
+#include <algorithm>
 using std::string;
 
-int main(){
-    string string1 = "a=1, b=7, c=3, d=14";
-    //oh boy
+int main() {
+    // Virtual CIN because I'm lazy
+    istringstream stringFull;
+    stringFull.str("a=1, b=7, c=3, d=14");
+
+    // Vector of strings because I'm lazy and don't want to worry about deleting the array or leave dangling pointers
+    vector<string> stringVector;
+
+    // Need temporary string in order to check if a push back is needed
+    string temp;
 
 
+    // Put all values into a string vector split by comma
+    while (getline(stringFull, temp, ','))
+        stringVector.push_back(temp);
 
+
+    // Clean the strings of non-numeric values.
+    for(int i = 0; i<stringVector.size();i++){
+        if (stringVector[i] != "") {
+            stringVector[i].erase(remove_if(stringVector[i].begin(), stringVector[i].end(), ::isspace),
+                                  stringVector[i].end());
+            stringVector[i].erase(remove_if(stringVector[i].begin(), stringVector[i].end(), ::ispunct),
+                                  stringVector[i].end());
+            stringVector[i].erase(remove_if(stringVector[i].begin(), stringVector[i].end(), ::isalpha),
+                                  stringVector[i].end());
+        }
+    }
+
+    // hahaha stoi = StringTOInteger... Why have I never seen this before? (is ATOI AutoTOInt?)
+    cout << "\na + b + c - d = " << stoi(stringVector[0])+stoi(stringVector[1])+stoi(stringVector[2])-stoi(stringVector[3]);
+
+
+//  Below did not work because I believe the new str is a "Copy" of the string not a pointer to it
+//  for (string str : stringVector) {
+//      str.erase(remove_if(str.begin(), str.end(), ::isspace), str.end());
+//      str.erase(remove_if(str.begin(), str.end(), ::ispunct), str.end());
+//      str.erase(remove_if(str.begin(), str.end(), ::isalpha), str.end());
+//  cout<<str<<":";
+//  }
+//
 }
 */
-
 /*
  * Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1. to 1000 digits
- */
+ *
 
 int main(){
 
@@ -182,4 +338,7 @@ int main(){
     fn1 = fn1+fn2;
     fn2 = fn1;
 }
+
+*/
+
 
