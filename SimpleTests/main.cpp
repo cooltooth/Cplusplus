c2aa56aef5d06e240a8cb99458f05ce62db38203
Daniel Hazelton <xooltooth@gmail.com>
2018-02-13 22:51:59 -0800

    feb 13

--- SimpleTests/main.cpp
+++ SimpleTests/main.cpp
@@ -1,4 +1,54 @@
-// thi of q = q/(1-q-q^2) <- inveres of that.
+/*Euler discovered the remarkable quadratic formula:
+
+n2+n+41
+It turns out that the formula will produce 40 primes for the consecutive integer values 0≤n≤39. However, when n=40,402+40+41=40(40+1)+41 is divisible by 41, and certainly when n=41,412+41+41 is clearly divisible by 41.
+
+The incredible formula n2−79n+1601 was discovered, which produces 80 primes for the consecutive values 0≤n≤79. The product of the coefficients, −79 and 1601, is −126479.
+
+Considering quadratics of the form:
+
+        n2+an+b, where |a|<1000 and |b|≤1000
+
+where |n| is the modulus/absolute value of n
+e.g. |11|=11 and |−4|=4
+Find the product of the coefficients, a and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
+*/
+
+
+
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int highPrime  = 0;
+    int primeCount = 0;
+
+
+    for(int i= -999;i<1000;i++)
+    {
+        bool isPrime = true;
+        for(int j=2;j<i;j++){
+            if (i % j == 0) {
+                //std::cout<<i<<":"<<j<<std::endl;
+                    isPrime = false;
+                    break;
+            }
+        }
+        if(isPrime) {
+            primeCount += i;
+        }
+
+
+
+
+        std::cout<<i<<std::endl;
+    }
+
+    return 0;
+}
+
+
+// phi^n/sqrt(5)>10^999 solve for n.
 /* 23, painfully slow, online versions can get it in 3 seconds..
  *
  * #include <iostream>
